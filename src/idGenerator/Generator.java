906828c4e0e335fc653ac7fb59fba623225d4c9e
2000159 <tlksimba519@gmail.com>
2020-07-01 16:19:40 +0800

    微調 加註解

--- src/idGenerator/Generator.java
+++ src/idGenerator/Generator.java
@@ -22,16 +22,23 @@ public class Generator {
 		int num1 = (int) (Math.random() * 10); int num2 = (int) (Math.random() * 10);
 		int num3 = (int) (Math.random() * 10); int num4 = (int) (Math.random() * 10);
 		int num5 = (int) (Math.random() * 10); int num6 = (int) (Math.random() * 10);
-		int num7 = (int) (Math.random() * 10);
+		int num7 = (int) (Math.random() * 10); //7碼流水碼 隨機產生
 		
+		/*
+		 * 身分證驗證演算法說明
+		 * 驗證 :將城市碼換算成數字後，總共11位數依序乘上指定的倍數加總後除以10，餘數為0即為合法身分證
+		 * 過程反推製造 :隨機產生流水碼，反求出最後一碼(即檢查碼)
+		 * 倍數表 :(1 9)城市碼 (8)性別碼    (7 6 5 4 3 2 1)流水碼   (1)檢查碼
+		 */
 		int numLast = (10 - ((((cityNum / 10) * 1) + ((cityNum % 10) * 9) + genderNum * 8 +
 				num1 * 7 + num2 * 6 + num3 * 5 + num4 * 4 + num5 * 3 + num6 * 2 + num7)% 10)) % 10;
 		
 		String newId = city + genderNum + num1 + num2 + num3 + num4 + num5 + num6 + num7+ numLast;
 		
+		//回傳前多驗證一次完整性
 		if(newId.matches("[A-Z]\\d{9}")) return newId; 
 		
-		Conversation.output("An error has occured");
+		Conversation.output("An error has occured.");
 		return null;
 		
 	}
--- src/utility/Converter.java
+++ src/utility/Converter.java
@@ -9,29 +9,26 @@ public class Converter {
 	
 	static {
 		
-		cityMap.put("A", 10); cityMap.put("B", 11); cityMap.put("C", 12);
-		cityMap.put("D", 13); cityMap.put("E", 14); cityMap.put("F", 15);
-		cityMap.put("G", 16); cityMap.put("H", 17); cityMap.put("I", 34);
-		cityMap.put("J", 18); cityMap.put("K", 19); //cityMap.put("L", 21);
-		cityMap.put("M", 21); cityMap.put("N", 22); cityMap.put("O", 35);
-		cityMap.put("P", 23); cityMap.put("Q", 24); //cityMap.put("R", 27);
-		/*cityMap.put("S", 28);*/ cityMap.put("T", 27); cityMap.put("U", 28);
-		cityMap.put("V", 29); cityMap.put("W", 32); cityMap.put("X", 30);
-		/*cityMap.put("Y", 34);*/ cityMap.put("Z", 33);
-		
+		cityMap.put("A", 10); cityMap.put("B", 11); cityMap.put("C", 12); //台北  台中  基隆
+		cityMap.put("D", 13); cityMap.put("E", 14); cityMap.put("F", 15); //台南  高雄  新北
+		cityMap.put("G", 16); cityMap.put("H", 17); cityMap.put("I", 34); //宜蘭  桃園  嘉義
+		cityMap.put("J", 18); cityMap.put("K", 19); cityMap.put("M", 21); //竹縣  苗栗  南投
+		cityMap.put("N", 22); cityMap.put("O", 35); cityMap.put("P", 23); //彰化  竹市  雲林
+		cityMap.put("Q", 24); cityMap.put("T", 27); cityMap.put("U", 28); //嘉義  屏東  花蓮
+		cityMap.put("V", 29); cityMap.put("W", 32); cityMap.put("X", 30); //台東  金門  澎湖
+		cityMap.put("Z", 33); //連江
+		//"L", "R", "S", "Y" 沒在用
 	}
 	
 	public static int convertCity(String city) {
 		
-		String regex = "[A-Z]";
-		
-		if(city.matches(regex)) {
+		if(city.matches("[A-Z]")) {
 			
 			try {
 				
 				return cityMap.get(city);
 				
-			} catch(NullPointerException e) {
+			} catch(NullPointerException e) { //抓LRSY
 				
 				Conversation.output("This city is no longer provide to use.");
 				System.exit(0);
@@ -51,7 +48,7 @@ public class Converter {
 	
 	public static int convertGender(String gender) {
 		
-		switch(gender.toUpperCase()) {
+		switch(gender.toUpperCase()) { //統一轉大寫方便驗證
 		
 			case "M" :
 				return 1;
